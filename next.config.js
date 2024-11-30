1136c1c4d88934b1b7845bce0e89443126905122
Megg <finero91@gmail.com>
2024-11-30 23:58:54 +0900

    Setup for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Static HTML 내보내기 설정
+  output: "export",
   basePath: process.env.NODE_ENV === "production" ? "/junto_cal_bot" : "",
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/junto_cal_bot/" : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
--- requirment.md
+++ requirment.md
@@ -4,3 +4,7 @@
 
 - 모든 css는 tailwindcss로 작성
 - javascript는 typescript로 작성
+
+2. 현재 에러
+
+- 모바일, 웨일에서 채팅창 안보임
