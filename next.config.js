93c9900a46d23876f546edeb343d0091b87574a8
Megg <finero91@gmail.com>
2024-11-30 23:22:12 +0900

    Setup for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export", // Static HTML 내보내기 설정
+  basePath: process.env.NODE_ENV === "production" ? "/junto_cal_bot" : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
